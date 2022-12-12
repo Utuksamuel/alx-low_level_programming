885d9b73f4c889aaf359ff68a42dd7e0381041a6
Utuksamuel <samuelutuk111@gmail>
2022-12-12 06:54:25 -0800

    Update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main -Entry Point
- * Description: Prints "\"Programming is like a multilingual puzzle"
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
  * Return: 0;
  */
 
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
