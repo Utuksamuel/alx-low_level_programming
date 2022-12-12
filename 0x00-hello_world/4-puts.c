390f41d9574e4830c0d1b02a93b8e239a858123a
Utuksamuel <samuelutuk111@gmail>
2022-12-12 06:46:54 -0800

    A programm that prints some strings

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main -Entry Point
+ * Description: Prints "\"Programming is like a multilingual puzzle"
+ * Return: 0;
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+}
