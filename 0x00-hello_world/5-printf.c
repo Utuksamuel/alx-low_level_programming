3ff2e9df53ffae1a3fe767514ed06aba0a4ac804
Utuksamuel <samuelutuk111@gmail>
2022-12-12 07:03:54 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints with proper grammmer, but the outcome is a piece of art
+ * Return: 0;
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
