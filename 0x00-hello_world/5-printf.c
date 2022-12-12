f47cb84457b2f1c3462fa33dd7df75761ceb1c71
Utuksamuel <samuelutuk111@gmail>
2022-12-12 07:06:09 -0800

    Quck fix for task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * Description: Prints with proper grammmer, but the outcome is a piece of art
+ * Description: Prints with proper grammmar, but the outcome is a piece of art
  * Return: 0;
  */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
