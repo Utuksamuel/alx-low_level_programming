5ff727162c5b01dfd408c605c14f429f9ca827b0
Utuksamuel <samuelutuk111@gmail>
2022-12-14 17:43:46 -0800

    quick fix for the first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
-* main -Entry point
-* prints Holberton
-* Return: Always 0 (success)
+*main - prins _putchar
+*
+* Return: always 0
 */
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
